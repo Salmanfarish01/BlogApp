38802203ca54e1a6c2c47c2e783ee7cacc6d2095
Salmanfarish01 <95988325+Salmanfarish01@users.noreply.github.com>
2024-11-13 17:38:07 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,14 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://deploy-mern-app.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true
+  }
+  
+));
 app.use(express.json());
 
 // MongoDB connection
